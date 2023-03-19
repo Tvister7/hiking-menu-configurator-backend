d2457b222b79792f6345c49d4d8603281521deee
Tvister7 <kirzap123@yandex.ru>
2023-03-19 05:22:54 +0300

    Reafctor settings

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,7 @@
 from uvicorn import run as run_fastapi
 
 from core.create_app import create_app
-from core.settings import settings
+from core.settings.base_settings import settings
 
 if __name__ == "__main__":
     run_fastapi(create_app(), host=settings.host)
